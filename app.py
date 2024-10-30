17f863cc6c85ba96bce131e51d3259f9e602a666
Srinivas H M <87597017+Seenuprime@users.noreply.github.com>
2024-10-30 21:41:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from forms import Query
+from forms import Query, Topic_Query
 from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
 import os
@@ -71,7 +71,12 @@ def home():
 
 def topic_prompt(topic):
     template = """
-       
+        Please provide an in-depth explanation of the topic '{topic}',.
+        Add some new lines to make it look cool use html use lower head tags from h3-h6 with 2 new lines for each topic.
+        Use pre tag only to show the code.
+        Covering its significance, core concepts, practical applications, challenges, and future trends and provide code if necessary. 
+        Offer clear and comprehensive information, structured to give a well-rounded understanding of the topic.
+        Give some sorces for each tipics to refer with direct like in 'a' tag.
     """
     prompt_template = PromptTemplate(
         input_variables=['topic'],
@@ -79,18 +84,18 @@ def topic_prompt(topic):
     )
     return prompt_template.format(topic=topic)
 
-topic_llm = ChatGroq()
+topic_llm = ChatGroq(model="llama3-groq-70b-8192-tool-use-preview")
 
 @app.route('/topics', methods=['GET', 'POST'])
 def topics():
-    topic_query = Query()
-    result = None
+    topic_query = Topic_Query()
+    topic_result = None
 
     if request.method == 'POST' and topic_query.validate_on_submit():
         topic_user_topic = topic_query.query.data
         topic_prompt_text = topic_prompt(topic_user_topic)
         topic_result = topic_llm.invoke(topic_prompt_text).content
-    return render_template('topics.html', topic_query=topic_query, top_result=topic_result)
+    return render_template('topics.html', topic_query=topic_query, topic_result=topic_result)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import Length
 
 class Query(FlaskForm):
     query = StringField('Query', validators=[Length(min=2, max=1000)])
+    submit = SubmitField("Ask")
+
+class Topic_Query(FlaskForm):
+    query = TextAreaField('Topic_Query', validators=[Length(min=2, max=1000)])
     submit = SubmitField("Ask")
